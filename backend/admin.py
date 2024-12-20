b727e376a36664f77a9d4ec0e4abd3d44fe6bea7
juliana <julianatko@gmail.com>
2024-12-20 00:27:05 -0300

    📦 NEW: implementando interface administrativa

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
 
-# Register your models here.
+from backend.models import URL, History, Product, Store
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('description',)  
+    search_fields = ('description',)  
+    
+admin.site.register(Store, StoreAdmin)
+
+class URLAdmin(admin.ModelAdmin):
+    list_display = ('store','created_at','url')  
+    search_fields = ('store',)  
+    
+admin.site.register(URL, URLAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('store','description',)  
+    search_fields = ('store',)  
+    
+    def has_add_permission(self, request):
+        return False 
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False 
+
+    
+admin.site.register(Product, ProductAdmin)
+
+class HistoryAdmin(admin.ModelAdmin):
+    list_display = ('at','default_price','offer_price','offer','category')  
+    search_fields = ('category',)  
+    list_filter = ('at','offer') 
+    
+    
+    def has_add_permission(self, request):
+        return False 
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False 
+admin.site.register(History, HistoryAdmin)
